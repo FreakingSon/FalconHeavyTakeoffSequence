6e36f4bfe20d370025e59db8909aa4ae44a8ce8f
unknown <elik@example.com>
2019-04-04 17:23:52 +0300

    0.01 version

--- Main/Main.js
+++ Main/Main.js
@@ -1,1 +1,42 @@
-Console.log('Eat a dick, nigga...');
+let IsTilted;
+let IsFueled;
+let IsCrewed;
+
+function initialization() {
+    let tilt = window.prompt("How tiled the rocket is on a lachpad (degrees)?");
+    if(tilt == 80)
+    {
+        IsTilted = true;
+    }
+
+    let fuel = window.prompt("Is it fueled yet?");
+    if(fuel == "yes")
+    {
+        IsFueled = true;
+    }
+
+    let fuel = window.prompt("Is crew locked and ready to go?");
+    if(fuel == "yes")
+    {
+        IsCrewed = true;
+    }
+
+    if(IsTilted == true)
+    {
+        Console.log("We're not ready to launch! Abort the mission or inspect the vehicle!");
+    }
+
+    if(IsFueled == false)
+    {
+        Console.log("We're not ready to launch! Abort the mission or inspect the vehicle!");
+    }
+
+    if(IsCrewed == false)
+    {
+        Console.log("We're not ready to launch! Abort the mission or inspect the vehicle!");
+    }
+
+    else{
+        Console.log("We're ready to count down!");
+    }
+} 
